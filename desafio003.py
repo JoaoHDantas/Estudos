b9f21ac2c4a851f2d8b07002de9d62ae6bbe7934
João Dantas <125670568+JoaoHDantas@users.noreply.github.com>
2023-03-03 15:03:11 -0300

    Add files via upload

---
+++ desafio003.py
@@ -0,0 +1,5 @@
+n1 = int(input('Primeiro Numero: '))
+n2 = int(input('Segundo Numero : '))
+soma = n1 + n2
+print('A soma entre {} e {} fica igual a: {}'.format(n1, n2, soma))
+# ,end='' para continuar a string e :numero para oder deicar um espaço entre as mascaras, < ^ >
---
+++ desafio008.py
@@ -0,0 +1,2 @@
+medida = int(input('Informe uma media para converter em CM e MM: '))
+print('{} Metros convertido para CM: {}cm, convertido para MM: {}mm'.format(medida, medida*100, medida*1000))
